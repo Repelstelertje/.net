cc87fed23a7ba9aeadd72dd033db38297c02394d
Repelstelertje <andreasalaerts@protonmail.com>
2025-07-03 12:56:27 +0200

    Fix vendor paths for bootstrap

--- SD/includes/header.php
+++ SD/includes/header.php
@@ -66,7 +66,7 @@
 ?>
 
 <!-- Bootstrap core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/vendor/bootstrap.min.css" rel="stylesheet">
 
 <!-- Custom styles for this template -->
 <link href="css/style.css" rel="stylesheet">
