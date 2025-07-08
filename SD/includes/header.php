a0f973c8dd5e0b29812424558feb056b43c2b807
Repelstelertje <andreasalaerts@protonmail.com>
2025-07-08 14:00:52 +0200

    fav

--- SD/includes/header.php
+++ SD/includes/header.php
@@ -42,24 +42,12 @@
 ?>
 <meta name="description" content="<?php echo htmlspecialchars($description, ENT_QUOTES, 'UTF-8'); ?>">
 <meta name="author" content="ShemaleDaten">
-
-<link rel="apple-touch-icon" sizes="57x57" href="img/fav/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="img/fav/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="img/fav/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="img/fav/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="img/fav/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="img/fav/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="img/fav/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="img/fav/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="img/fav/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="img/fav/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="img/fav/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="img/fav/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="img/fav/favicon-16x16.png">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="img/fav/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
-
+<link rel="icon" type="image/png" href="img/fav/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="img/fav/favicon.svg" />
+<link rel="shortcut icon" href="img/fav/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="img/fav/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="ShemaleDaten" />
+<link rel="manifest" href="img/fav/site.webmanifest" />
 <?php
   output_meta_tags($canonical, $pageTitle, $description, $ogImage);
 ?>
